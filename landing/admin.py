76b5df1fa791f6ac3640d3681846cec1f1207655
Panaev <28530@mail.ru>
2018-02-21 14:16:04 +0300

    Modify admin panel, rename db

--- landing/admin.py
+++ landing/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from landing.models import Subscriber
 
-admin.site.register(Subscriber)
 
+class SubscriberAdmin(admin.ModelAdmin):
+    class Meta():
+        model = Subscriber
+
+    list_display = [field.name for field in Subscriber._meta.fields]
+    list_filter = ['name']
+    search_fields = ['name', 'email']
+
+admin.site.register(Subscriber, SubscriberAdmin)
--- landing/models.py
+++ landing/models.py
@@ -5,8 +5,13 @@ from django.utils.encoding import smart_text
 # Create your models here.
 
 class Subscriber(models.Model):
-    email = models.EmailField()
+    class Meta:
+        db_table = 'subscribers'
+        verbose_name = 'Subscriber'
+        verbose_name_plural = 'Subscribers'
+
+    email = models.EmailField(unique=True)
     name = models.CharField(max_length=128)
 
     def __str__(self):
-        return smart_text(self.email)
+        return smart_text('№:'+str(self.id) + '\n Email:' + self.email)
