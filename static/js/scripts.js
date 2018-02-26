80c941dc441c552605b7ba5683e8ca8b1fdcf4b4
Panaev <28530@mail.ru>
2018-02-26 17:14:38 +0300

    Addition of goods to basket via JQuery

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,12 +3,40 @@ $(document).ready(function () {
     form.on('submit', function (e) {
             e.preventDefault();
             var count = $('#count').val();
-            var submit_btn = $('#submit_btn');
-            var product_id = submit_btn.data('product_id');
-            var product_name = submit_btn.data('name');
-            var product_price = submit_btn.data('price');
+            var submit_btn = $('#submit-btn');
+            var product_id = submit_btn.data("product_id");
+            var product_name = submit_btn.data("name");
+            var product_price = submit_btn.data("price");
+
+            $('.basket-items ul').append('<li>' + product_name + ', '
+                + count + ' шт.' + 'по ' + product_price + 'rub       ' +
+                '<a href="#" class="delete-item">x</a></li>')
+
         }
-    )
+    );
+
+    function showingBasket() {
+        $('.basket-items').removeClass('hidden');
+
+    }
+
+
+    $('.basket-container').on('click', function (e) {
+        e.preventDefault();
+        showingBasket()
+
+    });
+    $('.basket-container').mouseover(function (e) {
+        showingBasket()
+    });
+    // $('.basket-container').mouseout('click', function (e) {
+    //     $('.basket-items').addClass('hidden');
+    // });
+
+    $(document).on('click', '.delete-item', function (e) {
+        e.preventDefault();
+        $(this).closest('li').remove();
+    })
 });
 
 //TODO: https://www.youtube.com/watch?v=c2Q9wj9ju3Y
