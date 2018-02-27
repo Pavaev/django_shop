b188779ec0a6bd9d35b37c50b442e2fd25c41d9d
Panaev <28530@mail.ru>
2018-02-27 13:30:17 +0300

    Add correct basket update(only backend)

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -11,10 +12,17 @@ def basket_addition(request):
     data = request.POST
     product_id = data.get("product_id");
     count = data.get("count")
-    new_product = ProductInBasket.objects.create(session_key=session_key, product_id=product_id, count=count)
-    products_total_count = ProductInBasket.objects.filter(session_key=session_key, is_active=True).count()
-    return_dict["products_total_count"] = products_total_count
-    return JsonResponse(return_dict)
 
-
-
+    try:
+        product_existed = ProductInBasket.objects.get(session_key=session_key, product_id=product_id)
+        product_existed.count += int(count)
+        product_existed.save()
+    except ObjectDoesNotExist:
+        ProductInBasket.objects.create(session_key=session_key, product_id=product_id, count=count)
+    finally:
+        products = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
+        products_total_count = 0
+        for product in products:
+            products_total_count += product.count
+        return_dict["products_total_count"] = products_total_count
+        return JsonResponse(return_dict)
