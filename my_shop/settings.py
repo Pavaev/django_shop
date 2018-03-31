5bd2c4a52202047a80225bc11e0bf8d8a83198ad
Panaev <28530@mail.ru>
2018-03-31 12:56:14 +0300

    Vladick

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+#
 ALLOWED_HOSTS = ['localhost']
 
 # Application definition
